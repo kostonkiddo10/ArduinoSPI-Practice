65254261ca0b9eb9144c7d868341d30a23d1d6be
Davian Albarran <kostonkiddo10@gmail.com>
2020-02-10 18:18:49 -0500

    Getting char arr from arduino to RoboRio

--- Desktop/ArduinoSPI-Practice-master/ArduinoSPI-Practice-master/src/main/java/frc/robot/Pixy.java
+++ Desktop/ArduinoSPI-Practice-master/ArduinoSPI-Practice-master/src/main/java/frc/robot/Pixy.java
@@ -17,7 +17,7 @@ public class Pixy  {
 	public Pixy() {
 		pixy = new SPI(port);
 		pixy.setClockRate(9600);
-	//	pixy.initAuto(4);
+		pixy.initAuto(4);
 	//	pixy.setAutoTransmitData(new byte[0], 0);
 	//	pixy.startAutoRate(100.0);
 		//Must be called before starting SPI transfer engine, but the RoboRio
@@ -25,27 +25,15 @@ public class Pixy  {
 
 
 	public void pixyRead() {
-	//try {
-		//Never entered?
-		//System.out.println("About to read.");
-		pixy.read(true, buffer, 4);
-		//System.out.println(read);
 
-	/*	String v = new String( buffer.array(), StandardCharsets.UTF_8 );
-		System.out.println(v + "\nSomething printed.");
-		*/
+		//pixy.read(true, buffer, 4);
+
+	
 		for (int i = 0; i<4; i++){
 			System.out.println(i + " " + buffer.get(i));
 		}
-		//pixy.readAutoReceivedData(buffer, 0, 100);
-		//The method returns the amount of data not read, not sure what to do with this, though
-		//0 makes it read all the data available
-	//	return new String(buffer.array(), "UTF-8") + "hello";
-//}
-	/*catch(UnsupportedEncodingException e){
-		System.out.println("Exception: " + e.getMessage());
-	}
-	*/
+		pixy.readAutoReceivedData(buffer, 0, 100);
+		
 	}
 
 	public void pixyWrite(){
